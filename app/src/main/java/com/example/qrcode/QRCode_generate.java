615b913431b3e0f45df90131db1d030f424f567a
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-08 09:03:28 +0100

    QRCode Generating now works

--- app/src/main/java/com/example/qrcode/QRCode_generate.java
+++ app/src/main/java/com/example/qrcode/QRCode_generate.java
@@ -3,11 +3,19 @@ package com.example.qrcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class QRCode_generate extends AppCompatActivity {
     private EditText editText;
@@ -31,7 +39,21 @@ public class QRCode_generate extends AppCompatActivity {
         buttonQRGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
+                    if (editText.getText().toString().isEmpty()) {
+                        Toast.makeText(QRCode_generate.this,
+                                "Nem lehet üres a QRCode adat", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    BitMatrix bitMatrix = multiFormatWriter.encode(editText.getText().toString(),
+                            BarcodeFormat.QR_CODE, 500, 500);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    imageView.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
