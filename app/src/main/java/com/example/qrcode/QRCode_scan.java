bb45fdbba5d1c596c099d841d25f99476c1cd8ae
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-08 10:07:47 +0100

    QRCode Scanning now works

--- app/src/main/java/com/example/qrcode/QRCode_scan.java
+++ app/src/main/java/com/example/qrcode/QRCode_scan.java
@@ -1,17 +1,45 @@
 package com.example.qrcode;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class QRCode_scan extends AppCompatActivity {
     private Button buttonQRScan;
     private Button buttonScanBack;
     private TextView textView;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(
+                requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(QRCode_scan.this,
+                        "Kiléptél a QRCode olvasóból", Toast.LENGTH_SHORT).show();
+            } else {
+                textView.setText(result.getContents());
+                Uri uri = Uri.parse(result.getContents());
+                if (URLUtil.isValidUrl(result.getContents())){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
     public void init() {
         buttonQRScan = findViewById(R.id.buttonQRScan);
@@ -28,7 +56,12 @@ public class QRCode_scan extends AppCompatActivity {
         buttonQRScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                IntentIntegrator integrator = new IntentIntegrator(QRCode_scan.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("QRCode Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.initiateScan();
             }
         });
 
